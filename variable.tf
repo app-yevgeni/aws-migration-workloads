934a6fc447b7e9e40e944d7a19764a53cfaef511
app-yevgeni <23049337+app-yevgeni@users.noreply.github.com>
2026-06-25 10:41:52 +0300

    Add Terraform variables for Weaviate deployment
    Define variables for kubeconfig path, charts path, namespace, and API keys.

---
+++ variable.tf
@@ -0,0 +1,22 @@
+variable "kubeconfig_path" {
+  type        = string
+  description = "Path to kubeconfig for the target cluster"
+  default     = "~/.kube/config"
+}
+
+variable "charts_path" {
+  default     = "./charts"
+  description = "The charts full path"
+}
+
+variable "namespace" {
+  type        = string
+  default     = "weaviate"
+  description = "Kubernetes namespace where Weaviate is deployed"
+}
+
+variable "api_keys" {
+  type        = list(string)
+  default     = ["dev-key-123"]
+  description = "List of API keys for Weaviate authentication"
+}
